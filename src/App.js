ba9358c564e573ff3ebb6715ac55a9f4bc9bf11a
KarenWanyama3430 <you@example.com>
2024-07-19 22:45:53 +0300

    changes to the drivers view

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DriverView from '../src/components/DriverView';
+
+function App() {
+  return (
+    <div className="App">
+      <DriverView />
+    </div>
+  );
+}
+
+export default App;
